ffce771e64b4050b70a3cf7ed0f239d97b6520b7
ZXW834 <52708459+ZXW834@users.noreply.github.com>
2022-05-30 13:00:10 +0100

    Delete main_eff_tox_pack_markdown_and_shiny_apps.R

--- main_eff_tox_pack_markdown_and_shiny_apps.R
+++
@@ -1,7 +0,0 @@
-source("calcp_shiny_app.R")
-shinyApp(ui,server)
-
-if (pval>0){
-  rmarkdown::render("D:/University of Southampton/PhD Supervisor Dave and Dankmar/R/Rmarkdown/rstan/rstan/Eff-Tox_design_stan.Rmd")
-}
-
